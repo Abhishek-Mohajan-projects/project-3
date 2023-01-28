3eff256d89617cf168a57f2deac24a4546ae6717
Abhishek Mohajan <123286153+Abhishek-Mohajan-projects@users.noreply.github.com>
2023-01-28 12:16:41 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+
+});
